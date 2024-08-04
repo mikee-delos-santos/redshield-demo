4d8fc10678c0baea470d2c2aded6f46394d3346c
Mikee Delos Santos <mark.r.delos.santos@gmail.com>
2024-08-04 22:14:51 +0800

    Accordion 2

--- redshield-frontend/src/components/ActionButton.tsx
+++ redshield-frontend/src/components/ActionButton.tsx
@@ -3,7 +3,7 @@ type ActionButtonProps = {
 }
 
 function ActionButton(props: ActionButtonProps) {
-  return <div className="w-8 p-2 rounded-full bg-slate-300 flex justify-center">{props.icon}</div>
+  return <div className="w-[1.87rem] p-2 rounded-full bg-slate-300 flex justify-center">{props.icon}</div>
 }
 
 export default ActionButton
--- redshield-frontend/src/components/ContentRoute.tsx
+++ redshield-frontend/src/components/ContentRoute.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import ContentRouteType from "../types/ContentRouteType"
-import ActionButton from "./ActionButton"
 import { FiChevronRight } from "react-icons/fi"
+import './ContentRoute.css';
 
 type ContentRouteProps = {
   contentRoute: ContentRouteType
@@ -12,7 +12,9 @@ function ContentRoute(props: ContentRouteProps) {
 
   return <>
     <div key={props.contentRoute.id} className="grid grid-cols-[0.2fr_1fr_1fr_1fr_1fr_1fr_0.5fr] px-2 py-4 items-center">
-      <div><ActionButton icon={<FiChevronRight />}/></div>
+      <div onClick={() => setExpanded(!expanded) } className={`w-[1.87rem] p-2 rounded-full bg-slate-300 flex justify-center ${expanded ? "rotate-down" : ""}`}>
+        <FiChevronRight/>
+      </div>
       <div>{props.contentRoute.name}</div>
       <div>{props.contentRoute.serviceReference}</div>
       <div>{props.contentRoute.hostNames}</div>
@@ -20,6 +22,11 @@ function ContentRoute(props: ContentRouteProps) {
       <div>{props.contentRoute.spec}</div>
       <div>{props.contentRoute.locked ? 'Yes' : 'No'}</div>
     </div>
+
+    <section className={`${expanded ? "visible" : "hidden"}`}>
+
+
+    </section>
     <hr className="mx-[-20px]"/>
   </>
 
